37da7a032ce89a0b0b9184e8950b479b9fb5c9f9
Marilia Gama <mgama@opentable.com>
2024-09-26 22:48:19 -0700

    RecyclerView display asteroid list

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.udacity.asteroidradar.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.databinding.AsteroidItemBinding
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.domain.Asteroid
 
 class MainFragment : Fragment() {
 
@@ -23,6 +30,11 @@ class MainFragment : Fragment() {
         ViewModelProvider(this, MainViewModel.Factory(activity.application)).get(MainViewModel::class.java)
     }
 
+    /**
+     * RecyclerView Adapter for converting a list of Asteroids to list items
+     */
+    private var viewModelAdapter: AsteroidAdapter? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentMainBinding = DataBindingUtil.inflate(
@@ -35,6 +47,28 @@ class MainFragment : Fragment() {
 
         binding.viewModel = viewModel
 
+        viewModelAdapter = AsteroidAdapter(AsteroidClick {
+            // When an asteroid is clicked this block or lambda will be called by AsteroidAdapter
+
+            // context is not around, we can safely discard this click since the Fragment is no
+            // longer on the screen
+            val packageManager = context?.packageManager ?: return@AsteroidClick
+
+            // Try to generate a direct intent to the YouTube app
+            /*var intent = Intent(Intent.ACTION_VIEW, it.launchUri)
+            if(intent.resolveActivity(packageManager) == null) {
+                // YouTube app isn't found, use the web url
+                intent = Intent(Intent.ACTION_VIEW, Uri.parse(it.url))
+            }
+
+            startActivity(intent)*/
+        })
+
+        binding.root.findViewById<RecyclerView>(R.id.asteroid_recycler).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = viewModelAdapter
+        }
+
         setHasOptionsMenu(true)
 
         return binding.root
@@ -45,7 +79,7 @@ class MainFragment : Fragment() {
         viewModel.asteroidList.observe(viewLifecycleOwner) { asteroids ->
             Toast.makeText(context, "Asteroids count: " + asteroids.count(), Toast.LENGTH_LONG).show()
             asteroids?.apply {
-                //viewModelAdapter?.videos = asteroids
+                viewModelAdapter?.asteroids = asteroids
             }
         }
     }
@@ -57,5 +91,74 @@ class MainFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return true
+    }
+}
+
+/**
+ * Click listener for Asteroids. By giving the block a name it helps a reader understand what it does.
+ *
+ */
+class AsteroidClick(val block: (Asteroid) -> Unit) {
+    /**
+     * Called when an asteroid is clicked
+     *
+     * @param asteroid the asteroid that was clicked
+     */
+    fun onClick(asteroid: Asteroid) = block(asteroid)
+}
+
+/**
+ * RecyclerView Adapter for setting up data binding on the items in the list.
+ */
+class AsteroidAdapter(val callback: AsteroidClick) : RecyclerView.Adapter<AsteroidViewHolder>() {
+
+    /**
+     * The asteroids that our Adapter will show
+     */
+    var asteroids: List<Asteroid> = emptyList()
+        set(value) {
+            field = value
+            // Notify any registered observers that the data set has changed. This will cause every
+            // element in our RecyclerView to be invalidated.
+            notifyDataSetChanged()
+        }
+
+    /**
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidViewHolder {
+        val withDataBinding: AsteroidItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AsteroidViewHolder.LAYOUT,
+            parent,
+            false)
+        return AsteroidViewHolder(withDataBinding)
+    }
+
+    override fun getItemCount() = asteroids.size
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+     * position.
+     */
+    override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.asteroid = asteroids[position]
+            //it.videoCallback = callback
+        }
+    }
+
+}
+
+/**
+ * ViewHolder for asteroid items. All work is done by data binding.
+ */
+class AsteroidViewHolder(val viewDataBinding: AsteroidItemBinding) :
+    RecyclerView.ViewHolder(viewDataBinding.root) {
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.asteroid_item
     }
 }
